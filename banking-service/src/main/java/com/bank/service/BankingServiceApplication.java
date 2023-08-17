e6e0bc9a5beeedcc86213a4533b6e89ec4ee33cc
rammohanreddy147 pothula <rammohanreddy147@gmail.com>
2023-08-17 14:14:40 +0000

    TEST

--- banking-service/src/main/java/com/bank/service/BankingServiceApplication.java
+++ banking-service/src/main/java/com/bank/service/BankingServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankingServiceApplication {
     public static void main(String[] args) {
+
+        
         SpringApplication.run(BankingServiceApplication.class, args);
     }
 }
